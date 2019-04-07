b0fa3860772b643e931c8cae004efe314889b578
savoptik <sav70792@gmail.com>
2019-04-08 00:14:04 +0300

    Initial Commit

---
+++ calculateBrackets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calculateBrackets
+//
+//  Created by Артём Семёнов on 08/04/2019.
+//  Copyright © 2019 Артём Семёнов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
